3dee241719d8cc3c2be94e176b9edcb5d36db3c2
Kamil Żmijowski <kamz8@wp.pl>
2016-12-13 15:37:18 +0100

    dropFile
    Zrobiono prymitywne przechwytywanie pliku na serwer.

--- app/Http/Controllers/Admin/FilesController.php
+++ app/Http/Controllers/Admin/FilesController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
 
 use App\Http\Requests;
 
@@ -10,5 +15,27 @@ class FilesController extends Controller
 {
     public function index(){
         return view('admin.files');
+    }
+    
+    public function store(Request $request){
+      
+// if( $request->hasFile('droppedFiles') ) {
+        $file = $request->file('droppedFiles');
+        $extension = $file->getClientOriginalExtension();
+        $path = $file->move(public_path().'/upload/',$file->getClientOriginalName());
+        //Storage::disk('local')->put($file->getClientOriginalName(), file_get_contents($file));  
+        return Response::json([
+                    'success' => true,
+                    'message' => 'Dodano nowy plik!',
+                    'path'=>asset('upload/'.$file->getClientOriginalName())
+                ],200);        
+//    }  else return Response::json([
+//                    'success' => false,
+//                    'message' => 'Nie zapisano pliku!',
+//
+//                ],500);            
+       
+        
+                      
     }
 }
--- public/js/draggablezone.js
+++ public/js/draggablezone.js
@@ -1,4 +1,9 @@
 var $form = $('.box');
+var progressBar = '<div class="progress">\
+  <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width:0%">\
+    <span class="sr-only">45% Complete</span>\
+  </div>\
+</div>';
 var isAdvancedUpload = function() {
   var div = document.createElement('div');
   return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
@@ -18,8 +23,76 @@ if (isAdvancedUpload) {
     $form.removeClass('is-dragover');
   })
   .on('drop', function(e) {
-      
+                  
     droppedFiles = e.originalEvent.dataTransfer.files;
+                
+    var formFile = new FormData();
+	formFile.append('droppedFiles', droppedFiles[0]);    
+	$.ajax({
+	url: "api/files",
+	type: "POST",
+	data: formFile,
+	contentType:false,
+	cache: false,
+	processData: false,
+        mimeType:"multipart/form-data",
+        before: function() {
+
+            
+            
+        },
+	success: function(data){
+                $('.success').removeClass('hide');
+                $('#prosessing').addClass('hide');
+                $('#dropFile').find('.progress-bar').removeClass('active').addClass('progress-bar-success');
+                $('.box-image').remove();
+                                
+                var response = $.parseJSON(data);
+                $('.success').html(response.message);
+                $('.box-input').html('<div class="img-area">\
+    <img src="'+response.path+'">\
+    <button type="button" class="close" aria-label="Close"><span aria-hidden="true">×</span></button>\
+</div>');        
+	},
+        error: function(data) {
+                        
+        },
+           complete: function(data){
+           var response = $.parseJSON(data);    
+    
+        },
+        xhr: function() {
+                var xhr = new window.XMLHttpRequest();
+                
+                $('.box-input').append(progressBar);
+                $('#prosessing').removeClass('hide');
+                $('.box-input label').remove();
+                xhr.upload.addEventListener("progress", function(evt) {
+                    if (evt.lengthComputable) {
+                        var percentComplete = evt.loaded / evt.total;
+                        //Do something with upload progress here
+                        $('#dropFile').find('.progress-bar').css({'width':(percentComplete*100)+'%'});
+                        console.log(percentComplete);
+                    }
+               }, false);
+
+               xhr.addEventListener("progress", function(evt) {
+                   if (evt.lengthComputable) {
+                       var percentComplete = evt.loaded / evt.total;
+                       //Do something with download progress
+                       console.log(percentComplete);
+                   }
+               }, false);
+
+               return xhr;
+            }     
+    
+      });
   });
 
+
 }
+
+$(document).on('click','button.close',function () {
+    $('.img-area').remove();
+});
