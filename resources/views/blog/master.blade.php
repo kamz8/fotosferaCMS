78cd5880f90628b80ad33699bcd86b6b31f42c65
Kamil Żmijowski <kamz8@wp.pl>
2017-08-29 14:50:46 +0200

    lightbox
    Widok dla lightboxa w galeri zdjęć
    Podstawowe UI

--- resources/views/blog/master.blade.php
+++ resources/views/blog/master.blade.php
@@ -24,7 +24,8 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
         <script src="{{asset('js/vendor/modernizr-2.8.3-respond-1.4.2.min.js')}}"></script>   
         <link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}">
-        <link rel="stylesheet" href="{{asset('css/bootstrap-social.css')}}">        
+        <link rel="stylesheet" href="{{asset('css/bootstrap-social.css')}}">  
+        @stack('stylesheet')
         <meta name="csrf-token" content="{{ csrf_token() }}" />
     </head>
 
